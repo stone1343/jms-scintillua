7b1dcbdd3a30ee8d91649388d0c44a3ded6f36ff
Jeff Stone <stone1343@gmail.com>
2018-10-21 20:51:20 -0400

    More tweaks

--- install.bat
+++ install.bat
@@ -8,7 +8,6 @@ if "%1"=="" (
 if not exist %1\ (
   mkdir %1
 )
-if not exist %1\. mkdir %1
 xcopy /s /y windows\* %1
 xcopy /s /y scintillua\* %1
 xcopy /y scintillua\SciTEUser.properties %USERPROFILE%
