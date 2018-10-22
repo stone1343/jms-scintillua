f96ca1362acf8f8cfe57623d59c9d999f925444c
Jeff Stone <stone1343@gmail.com>
2018-10-21 20:00:32 -0400

    More tweaks

--- install.bat
+++ install.bat
@@ -6,22 +6,7 @@ if "%1"=="" (
   goto :eof
 )
 
-xcopy /y /d windows\* %1
-xcopy /y /d scintillua\* %1
-call :smartfilecopy windows\SciTEGlobal.properties %1\SciTEGlobal.properties
-call :smartfilecopy windows\SciTEUser.properties %USERPROFILE%\SciTEUser.properties
-goto :eof
-
-:smartfilecopy
-set doit=
-rem echo %~n0 %1 %2
-if not exist %2 (
-  set doit=y
-) else (
-  fc /l %1 %2 >nul || set doit=y
-)
-if "%doit%"=="y" (
-  echo copy %1 %2
-  copy %1 %2 >nul
-)
-goto :eof
+if not exist %1\. mkdir %1
+xcopy /s /y windows\* %1
+xcopy /s /y scintillua\* %1
+xcopy /y scintillua\SciTEUser.properties %USERPROFILE%
