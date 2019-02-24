3c656db730ba6c36f6124b0ace3818cf8a7d61d1
Jeff Stone <stone1343@gmail.com>
2019-02-24 15:52:14 -0500

    Separate build and install

--- update.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo cp scintillua/startup.lua /usr/share/scite
-sudo cp -r scintillua/lexers /usr/share/scite
-sudo cp linux/SciTEGlobal.properties /usr/share/scite
-cp linux/SciTEUser.properties $HOME/.SciTEUser.properties
